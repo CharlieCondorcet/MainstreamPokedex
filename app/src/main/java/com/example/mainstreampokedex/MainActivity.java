ed61b20c9a0dd54d9b975f61f39288417a22b4a7
CharlieCondorcet <charliecondorcet@gmail.com>
2020-05-25 19:28:09 -0400

    add scroll to charge only necesary images

--- app/src/main/java/com/example/mainstreampokedex/MainActivity.java
+++ app/src/main/java/com/example/mainstreampokedex/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mainstreampokedex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -23,10 +24,14 @@ public class MainActivity extends AppCompatActivity {
 
     private Retrofit retrofit;
 
-    private static final String TAG ="POKEDEX";
+    private static final String TAG = "POKEDEX";
 
     private RecyclerView recyclerView;
     private ListaPokemonAdapter listaPokemonAdapter;
+
+    private int offset;
+
+    private boolean aptoParaCargar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,23 +45,49 @@ public class MainActivity extends AppCompatActivity {
         GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
         recyclerView.setLayoutManager(layoutManager);
 
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                if (dy > 0) {
+                    int visibleItemCount = layoutManager.getChildCount();
+                    int totalItemCount = layoutManager.getItemCount();
+                    int pastVisibleItems = layoutManager.findFirstVisibleItemPosition();
+
+                    if (aptoParaCargar) {
+
+                        if ((visibleItemCount + pastVisibleItems) >= totalItemCount) {
+                            Log.i(TAG, "Fin de la lista de pokemon!");
+                            aptoParaCargar = false;
+                            offset += 20;
+                            obtenerDatos(offset);
+                        }
+                    }
+                }
+            }
+        });
+
         retrofit = new Retrofit
                 .Builder()
                 .baseUrl("https://pokeapi.co/api/v2/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        obtenerDatos();
+        aptoParaCargar = true;
+        this.offset = 0;
+        obtenerDatos(offset);
     }
 
-    public void obtenerDatos() {
+    public void obtenerDatos(int offset) {
         PokeapiService service = retrofit.create(PokeapiService.class);
-        Call<PokemonRespuesta> pokemonRespuestaCall = service.obtenerListaPokemon();
+        Call<PokemonRespuesta> pokemonRespuestaCall = service.obtenerListaPokemon(20, offset);
 
         pokemonRespuestaCall.enqueue(new Callback<PokemonRespuesta>() {
             @Override
             public void onResponse(Call<PokemonRespuesta> call, Response<PokemonRespuesta> response) {
-                if(response.isSuccessful()) {
+                aptoParaCargar = true;
+                if (response.isSuccessful()) {
 
                     PokemonRespuesta pokemonRespuesta = response.body();
                     ArrayList<Pokemon> listaPokemon = pokemonRespuesta.getResults();
@@ -70,7 +101,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<PokemonRespuesta> call, Throwable t) {
-                Log.i(TAG,"onFailure: "+ t.getMessage());
+                aptoParaCargar = true;
+                Log.i(TAG, "onFailure: " + t.getMessage());
             }
         });
 
